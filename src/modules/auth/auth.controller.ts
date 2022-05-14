5f38d8d8194be91c09d41e22eb9de43541d154b9
Julio Herrera <cesarchavarria0@gmail.com>
2022-05-14 15:47:39 -0600

    Cleaning up authController

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,26 +1,10 @@
-import {
-  Controller,
-  Post,
-  Res,
-  HttpStatus,
-  Get,
-  HttpCode,
-  Body,
-  UsePipes,
-  ValidationPipe,
-} from '@nestjs/common';
-import { Response } from 'express';
+import { Controller, Post, Get, HttpCode, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignUpUserDto } from './dto/signUpUser.dto';
 import { SignInUserDto } from './dto/signInUser.dto';
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
-
-  @Get('test')
-  @HttpCode(200)
-  test() {}
-
   @Post('sign-up')
   @HttpCode(201)
   signUp(@Body() userData: SignUpUserDto) {
