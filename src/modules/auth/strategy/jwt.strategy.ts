95443a28ddea85fdd4f55266791f94f572c8b438
Julio Herrera <cesarchavarria0@gmail.com>
2022-05-14 15:46:11 -0600

    Improvements on JWT security

--- src/modules/auth/strategy/jwt.strategy.ts
+++ src/modules/auth/strategy/jwt.strategy.ts
@@ -2,13 +2,23 @@ import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UsersRepository } from 'src/repositories/users.repository';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private config: ConfigService) {
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(
+    config: ConfigService,
+    @InjectRepository(UsersRepository) private userRepository: UsersRepository,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: config.get('JWT_SECRET'),
     });
+  }
+  async validate(payload: { sub: number; email: string }) {
+    const user = await this.userRepository.findOne(payload.sub);
+    delete user.password;
+    return user;
   }
 }
